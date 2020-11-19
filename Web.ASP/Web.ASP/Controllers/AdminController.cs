c79fc48d9317aeaae9497852b790148390c0ac86
quynhbud <nhuquynhnguyen2811@gmail.com>
2020-11-19 23:15:44 +0700

    add book in database

--- Web.ASP/Web.ASP/Controllers/AdminController.cs
+++ Web.ASP/Web.ASP/Controllers/AdminController.cs
@@ -52,5 +52,114 @@ namespace Web.ASP.Controllers
             ViewBag.publishingHouseBook = new SelectList(db.PUBLISHING_HOUSE, "C_id", "namePublishingHouse");
             return PartialView();
         }
+        public ActionResult ValidateBook( string nameBook, string contentBook,string categoryBook_ID,
+                                    string publishingHouseBook_ID, string countBook, string priceBook)
+        {
+            if (String.IsNullOrEmpty(nameBook))
+            {
+                var result = new
+                {
+                    status = false,
+                    message = "Vui lòng nhập tên sách"
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            if (String.IsNullOrEmpty(contentBook))
+            {
+                var result = new
+                {
+                    status = false,
+                    message = "Vui lòng nhập mô tả sách"
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            if (String.IsNullOrEmpty(contentBook))
+            {
+                var result = new
+                {
+                    status = false,
+                    message = "Vui lòng nhập mô tả sách"
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            if (String.IsNullOrEmpty(categoryBook_ID))
+            {
+                var result = new
+                {
+                    status = false,
+                    message = "Vui lòng chọn thể loại sách"
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            if (String.IsNullOrEmpty(publishingHouseBook_ID))
+            {
+                var result = new
+                {
+                    status = false,
+                    message = "Vui lòng chọn nhà xuất bản"
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            if (String.IsNullOrEmpty(countBook))
+            {
+                var result = new
+                {
+                    status = false,
+                    message = "Vui lòng nhập số lượng sách"
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            if (String.IsNullOrEmpty(priceBook))
+            {
+                var result = new
+                {
+                    status = false,
+                    message = "Vui lòng nhập giá sách"
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            var book = db.BOOKs.Find(nameBook);
+            if(!(book is null))
+            {
+                var result = new
+                {
+                    status = false,
+                    link = new
+                    {
+                        actionName = "AddBook",
+                        controllerName = "Admin"
+                    },
+                    message = "Nhập sách không thành công"
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                var addbook = new BOOK();
+                addbook.C_id = "MS11";
+                addbook.nameBook = nameBook;
+                addbook.priceBook = Int32.Parse(priceBook);
+                addbook.contentBook = contentBook;
+                addbook.countBook = Int32.Parse(countBook); ;
+                addbook.imgBook_ID = "img01";
+                
+                addbook.categoryBook_ID = categoryBook_ID;
+                
+                addbook.publishingHouseBook_ID = publishingHouseBook_ID;
+                db.BOOKs.Add(addbook);
+                db.SaveChanges();
+                var result = new
+                {
+                    status = true,
+                    link = new
+                    {
+                        actionName = "AddBook",
+                        controllerName = "Admin"
+                    },
+                    message = "Nhập sách thành công"
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
     }
 }
